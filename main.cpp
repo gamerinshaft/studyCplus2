28292c79b4dd42834c1130d7f26252064ffc0e4c
j.taneichi <j.tane3ma@gmail.com>
2015-11-10 16:42:39 +0900

    type of int

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main() {
     string name;
+    int age;
     cout << "Please input your name." << endl;
     cin >> name;
     cout << "your name is " << name << "." << endl;
+    cout << "Please tell me your age." << endl;
+    cin >> age;
+    cout << "your age is " << age << endl;
     return 0;
 }
